c69cf0110d9dc567ae5488b84ee3625e279f792f
ArunaChandru <157276742+ArunaChandru@users.noreply.github.com>
2024-01-24 12:29:05 +0530

    Patient.java

--- src/hospital/Patient.java
+++ src/hospital/Patient.java
@@ -12,21 +12,28 @@ public class Patient {
         this.scanner = scanner;
     }
 
-    public void addPatient(){
+    public void addPatient()
+    {
         System.out.print("Enter Patient Name: ");
         String name = scanner.next();
+        
         System.out.print("Enter Patient Age: ");
         int age = scanner.nextInt();
+        
         System.out.print("Enter Patient Gender: ");
         String gender = scanner.next();
 
         try{
             String query = "INSERT INTO patients(name, age, gender) VALUES(?, ?, ?)";
+            
             PreparedStatement preparedStatement = connection.prepareStatement(query);
+            
             preparedStatement.setString(1, name);
             preparedStatement.setInt(2, age);
             preparedStatement.setString(3, gender);
+            
             int affectedRows = preparedStatement.executeUpdate();
+            
             if(affectedRows>0){
                 System.out.println("Patient Added Successfully!!");
             }else{
@@ -38,15 +45,20 @@ public class Patient {
         }
     }
 
-    public void viewPatients(){
+    public void viewPatients()
+    {
         String query = "select * from patients";
+        
         try{
             PreparedStatement preparedStatement = connection.prepareStatement(query);
+            
             ResultSet resultSet = preparedStatement.executeQuery();
+            
             System.out.println("Patients: ");
             System.out.println("+------------+--------------------+----------+------------+");
             System.out.println("| Patient Id | Name               | Age      | Gender     |");
             System.out.println("+------------+--------------------+----------+------------+");
+            
             while(resultSet.next()){
                 int id = resultSet.getInt("id");
                 String name = resultSet.getString("name");
@@ -61,12 +73,17 @@ public class Patient {
         }
     }
 
-    public boolean getPatientById(int id){
+    public boolean getPatientById(int id)
+    {
         String query = "SELECT * FROM patients WHERE id = ?";
+        
         try{
             PreparedStatement preparedStatement = connection.prepareStatement(query);
+            
             preparedStatement.setInt(1, id);
+            
             ResultSet resultSet = preparedStatement.executeQuery();
+            
             if(resultSet.next()){
                 return true;
             }else{
